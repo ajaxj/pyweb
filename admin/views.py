8442b762be69e51cf2d72d415ad8a0bea5e1cfef
ajaxj <ajaxj@qq.com>
2013-07-15 12:17:00 +0800

    主项目开始为高尔夫手机后台作准备

--- admin/views.py
+++ admin/views.py
@@ -6,3 +6,13 @@ from admin import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+def users():
+    return render_template('users')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
